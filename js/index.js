e4b4089a9af8cdf5b2c5ebcc5afbdae158415adf
GOKUL.A.N <113367457+GOKUL-A-N@users.noreply.github.com>
2024-07-20 16:24:48 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function handleSubmit() {
+    const content = document.getElementById('inputs').value;
+    onUpgrade(content)
+}
+let myList = document.querySelector("#addcontent");
+function onUpgrade(content) {
+    const newContent = document.createElement("li"); 
+    newp = document.createElement("p");
+    newp.textContent = content; 
+    newinput = document.createElement("input");
+    newContent.appendChild(newinput);
+    newContent.appendChild(newp);
+    newContent.className="w-[100%] rounded-md h-[50px] bg-[#bcef4c] font-semibold flex items-center justify-start gap-3"
+    newinput.className="ml-3 h-[20px] rounded-full outline-none w-[20px]";
+    newp.className="w-[80%]"
+    newinput.type="checkbox"
+    var delButton = document.createElement("button");
+    delButton.textContent = "Delete";
+    delButton.className = "mr-4 hover:animate-pulse"
+    delButton.addEventListener("click", function() {
+      myList.removeChild(newContent);
+    });
+    newContent.appendChild(delButton);
+
+    myList.appendChild(newContent);
+}
+
